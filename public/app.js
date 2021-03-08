d1b67e2b0929c23b68648b67e7fd4213a27a649b
maiquel-mendes <maiquelbsb@gmail.com>
2021-03-08 10:33:44 -0300

    update

--- public/app.js
+++ public/app.js
@@ -1,7 +1,3 @@
-// const firebase = require("firebase");
-// // Required for side-effects
-// require("firebase/firestore");
-
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -19,13 +15,13 @@ var db = firebase.firestore(app)
 
 
 const tarefa = document.querySelector('#tarefa')
-let liElement;
+var liElement;
 var ulElement = document.querySelector('.lista')
-let retorno = JSON.parse(localStorage.getItem('array'));
+var teste = document.querySelector('.8jcaYf67i8T9MBFukhWO')
+// console.log(teste)
 
 
 function render() {
-
 
     db.collection("tarefas").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
@@ -35,20 +31,7 @@ function render() {
             criarElemento(dados.task, doc.id);
         });
     });
-    // if (retorno) {
 
-
-    //     for (let item of retorno) {
-
-
-    //         criarElemento(item);
-    //     }
-
-    // } else {
-    //     localStorage.setItem('array', JSON.stringify([]));
-    //     retorno = JSON.parse(localStorage.getItem('array'));
-
-    // }
 }
 render()
 
@@ -83,10 +66,6 @@ function cad() {
             console.error("Error adding document: ", error);
         });
     // [END add_ada_lovelace]
-    // retorno.push(tarefa.value)
-    // localStorage.setItem('array', JSON.stringify(retorno))
-
-
 
 }
 
@@ -99,28 +78,22 @@ function criarElemento(value, id) {
     var botao = document.createElement('button')
     botao.innerHTML = '<i class="fa fa-trash"></i>'
     botao.className = 'ml-4 px-2 py-2 items-center border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700'
-    var pos = retorno.indexOf(value)
-    console.log(id)
-    botao.setAttribute('onclick', `deleteTodo("${id}")`)
+
+    console.log("teste")
+    botao.setAttribute('onclick', `deleteTodo(${id})`)
     liElement.appendChild(botao)
 
 }
 
 function deleteTodo(pos) {
-    // retorno.splice(pos, 1)
-    // localStorage.setItem('array', JSON.stringify(retorno))
+
     let liEle = document.querySelector(`.${pos}`)
     console.log(liEle)
     db.collection("tarefas").doc(`${pos}`).delete().then(() => {
         console.log("Document successfully deleted!");
-        // ulElement.innerHTML = ''
         ulElement.removeChild(liEle)
-        // render()
     }).catch((error) => {
         console.error("Error removing document: ", error);
-        render()
     });
-
-
 
 }
