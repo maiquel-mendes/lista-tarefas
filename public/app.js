35d5b1bb44ea7847778842fa471a9fdac71f9fa7
maiquel-mendes <maiquelbsb@gmail.com>
2021-03-10 12:18:26 -0300

    update checkbox

--- public/app.js
+++ public/app.js
@@ -25,9 +25,10 @@ function render() {
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
             let dados = doc.data();
-            // console.log(doc.id)
-            criarElemento(dados.task, doc.id);
+            // console.log(dados.concluida)
+            criarElemento(dados.task, doc.id, dados.concluida);
         });
+        verificaTask()
     });
 
 }
@@ -52,27 +53,31 @@ function cad() {
         task: tarefa.value,
         concluida: false,
 
-
     })
         .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
+            console.log("Document written with ID: ", docRef.id, false);
             id = docRef.id;
-            criarElemento(tarefa.value, id);
+            criarElemento(tarefa.value, id, false);
             tarefa.value = ''
+            verificaTask()
         })
         .catch((error) => {
             console.error("Error adding document: ", error);
         });
-    // [END add_ada_lovelace]
 
 }
 
-function criarElemento(value, id) {
+function criarElemento(value, id, check) {
 
     liElement = document.createElement('li')
+    checkElement = document.createElement('input')
+    checkElement.setAttribute('type', 'checkbox')
+    checkElement.setAttribute('id', `${id}`)
+
     liElement.innerHTML = value;
     liElement.setAttribute('data-js', `${id}`)
     liElement.className = 'd-flex list-group-item justify-content-between'
+    checkElement.className = 'd-flex'
     ulElement.appendChild(liElement)
     var botao = document.createElement('button')
     botao.innerHTML = '<i class="bi bi-trash"></i>'
@@ -80,8 +85,48 @@ function criarElemento(value, id) {
     botao.addEventListener('click', function () {
         deleteTodo(id)
     })
+
+    liElement.appendChild(checkElement)
+    if (check) {
+        checkElement.checked = true
+
+    }
     liElement.appendChild(botao)
 
+}
+
+function verificaTask() {
+
+
+    let xeqEle = document.querySelectorAll(`li input`)
+    xeqEle.forEach(item => {
+        let text = item.parentNode
+        riscaTexto(item, text)
+
+        item.addEventListener('click', function (e) {
+
+            db.collection("tarefas").doc(`${item.id}`).update({
+                concluida: item.checked
+            }).then(() => {
+                riscaTexto(item, text)
+                console.log("Document successfully update!", item.checked);
+            }).catch((error) => {
+                console.error("Error removing document: ", error);
+            });
+        })
+    })
+
+}
+
+function riscaTexto(item, texto) {
+    if (item.checked === true) {
+        texto.classList.add('text-decoration-line-through')
+        console.log('o resultado true', item.checked)
+    } else {
+        texto.classList.remove('text-decoration-line-through')
+        console.log('o resultado falso', item.checked)
+
+    }
 }
 
 function deleteTodo(pos) {
